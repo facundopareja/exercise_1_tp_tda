bd66a811361983cf2002ee0f54733489777366ca
facundopareja <facundopareja@gmail.com>
2024-09-03 00:47:50 -0300

    improved parsers

--- parsers.py
+++ parsers.py
@@ -3,7 +3,7 @@ def parse_tasks_file(filename):
     with open(filename, "r") as file:
         for line in file:
             split_line = line.split(',')
-            required_tasks[(int(split_line[0]), split_line[1])] = convert_to_int(split_line[2::])
+            required_tasks[(int(split_line[0]), split_line[1])] = list(map(int, split_line[2::]))
     return required_tasks
 
 def parse_earnings_file(filename):
@@ -11,10 +11,5 @@ def parse_earnings_file(filename):
     with open(filename, "r") as file:
         for line in file:
             split_line = line.split(',')
-            potential_earnings[int(split_line[0])] = convert_to_int(split_line[1::])
+            potential_earnings[int(split_line[0])] = list(map(int, split_line[1::]))
     return potential_earnings
-
-def convert_to_int(elements_list):
-    for i, item in enumerate(elements_list):
-        elements_list[i] = int(item)
-    return elements_list
